d26e9dc9e7bf10cf78cce947836787d897022261
MaxRamirez <leorexx100@gmail.com>
2022-03-16 11:04:22 -0400

    uniformacion de filtrado de tablas v1

--- resources/assets/js/layout/Appbar.vue
+++ resources/assets/js/layout/Appbar.vue
@@ -1,15 +1,9 @@
 <template>
-  <v-app-bar
-    app
-    fixed
-    dense
-    flat
-    clipped-left
-    dark
-    :color="bar.color"
-  >
+  <v-app-bar app fixed dense flat clipped-left dark :color="bar.color">
     <template v-if="rolePermissionSelected">
-      <v-app-bar-nav-icon @click.stop="$emit('update:expanded', !expanded)"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon
+        @click.stop="$emit('update:expanded', !expanded)"
+      ></v-app-bar-nav-icon>
     </template>
     <v-toolbar-title>{{ bar.text }}</v-toolbar-title>
     <v-spacer></v-spacer>
@@ -25,96 +19,104 @@
         return-object
         dense
       ></v-select> -->
-      <span class="text-caption font-weight-bold">{{ rolePermissionSelected ? rolePermissionSelected.display_name : '' }}</span>
-        <v-btn
-          fab
-          dark
-          x-small
-          v-if="rolePermissionSelected!=null"
-          color="white"
-          outlined
-          class="mx-3"
-          @click="$router.push('/changeRol')">
-          <v-icon>mdi-keyboard-return</v-icon>
-        </v-btn>
+      <span class="text-caption font-weight-bold">{{
+        rolePermissionSelected ? rolePermissionSelected.display_name : ""
+      }}</span>
+      <v-btn
+        fab
+        dark
+        x-small
+        v-if="rolePermissionSelected != null"
+        color="white"
+        outlined
+        class="mx-3"
+        @click="$router.push('/changeRol')"
+      >
+        <v-icon>mdi-keyboard-return</v-icon>
+      </v-btn>
     </div>
-    <LoggedUser/>
+    <LoggedUser />
   </v-app-bar>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import LoggedUser from '@/layout/LoggedUser'
+import { mapGetters } from "vuex";
+import LoggedUser from "@/layout/LoggedUser";
 
 export default {
-  name: 'app-mainbar',
+  name: "app-mainbar",
   components: {
-    LoggedUser
+    LoggedUser,
   },
   props: {
     expanded: {
       type: Boolean,
-      default: false
-    }
+      default: false,
+    },
   },
   data() {
     return {
       rolesPermissionsItems: [],
-    }
+    };
   },
   async created() {
-    await this.getRolePermissions()
+    await this.getRolePermissions();
     // this.setDefaultValues()
   },
   methods: {
     async getRolePermissions() {
       try {
-        let res = await axios.get(`user_role/permission`)
-        let aux_rolesPermissionsItems = res.data
-        aux_rolesPermissionsItems.forEach(item => {
+        let res = await axios.get(`user_role/permission`);
+        let aux_rolesPermissionsItems = res.data;
+        aux_rolesPermissionsItems.forEach((item) => {
           //delete item.id
-          delete item.module_id
-          delete item.action
-          delete item.created_at
-          delete item.updated_at
-          delete item.correlative
-          delete item.name
-          delete item.sequence_number
-          item.permissions = item.permissions.map(item => ({display_name: item.display_name, name: item.name }))
-        })
-        this.rolesPermissionsItems =  aux_rolesPermissionsItems
+          delete item.module_id;
+          delete item.action;
+          delete item.created_at;
+          delete item.updated_at;
+          delete item.correlative;
+          delete item.name;
+          delete item.sequence_number;
+          item.permissions = item.permissions.map((item) => ({
+            display_name: item.display_name,
+            name: item.name,
+          }));
+        });
+        this.rolesPermissionsItems = aux_rolesPermissionsItems;
         //console.log(this.rolesPermissionsItems)
       } catch (e) {
-        console.log(e)
+        console.log(e);
       }
     },
     setDefaultValues() {
-      if(this.rolesPermissionsItems.length > 0) {
-        this.rolePermissionSelected = this.rolesPermissionsItems[0]
+      if (this.rolesPermissionsItems.length > 0) {
+        this.rolePermissionSelected = this.rolesPermissionsItems[0];
       }
     },
   },
   computed: {
-    ...mapGetters(['rolePermissionSelected']),
+    ...mapGetters(["rolePermissionSelected"]),
     bar() {
-      if (process.env.NODE_ENV != 'production') {
+      if (process.env.NODE_ENV != "production") {
         return {
           color: `error`,
-          text: `PLATAFORMA VIRTUAL DE TRÁMITES (VERSIÓN DE PRUEBA)`
-        }
+          text: `PLATAFORMA VIRTUAL DE TRÁMITES (VERSIÓN DE PRUEBA21234)`,
+        };
       } else {
         return {
           color: `primary`,
-          text: `PLATAFORMA VIRTUAL DE TRÁMITES`
-        }
+          text: `PLATAFORMA VIRTUAL DE TRÁMITESasdasd`,
+        };
       }
     },
-
   },
   watch: {
-    'rolePermissionSelected.display_name'(val) {
-      this.$store.commit('setRolePermissionSelected', this.rolePermissionSelected)
-    }
-  }
-}
+    "rolePermissionSelected.display_name"(val) {
+      this.$store.commit(
+        "setRolePermissionSelected",
+        this.rolePermissionSelected
+      );
+    },
+  },
+};
 </script>
